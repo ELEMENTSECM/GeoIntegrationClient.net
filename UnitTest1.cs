71a3f587f22504dc5df2acf0863d54ce3d211a2c
Steinar Herland <steinar.herland@evry.com>
2019-11-01 10:47:01 +0100

    File | New | TestProject

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeoIntegrationClient
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
